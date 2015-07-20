0ce44073b52cdd8e4c48f29ddca83ec2f9286e8c
lc4t <lc4t0.0@gmail.com>
2015-07-20 22:09:42 +0800

    Web1

--- web1/web1gogogo.php
+++ web1/web1gogogo.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lc4t
- * Date: 7/19/15
- * Time: 11:37 PM
- */
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if ($password == "" )
+    {
+        if ($username == "admin" || $username == "root" || $username == "system")
+        {
+            print "Yep, but I can't trust you...<br>";
+        }
+        else
+        {
+            print "Hey,don't hack me!<br>";
+        }
+    }
+
+    else if ($username == "root" && $password == "toor")
+    {
+        print "Enn, you guess is right<br>";
+        print "but there is no egg use";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "<br><br><br><br><br><br><br><br><br><br><br><br>";
+        print "CNSS{Sooooo_easy}";
+        print "<!-- 434E53537B49745F69735F456173797D -->";//CNSS{It_is_Easy}
+    }
+
+    else
+    {
+        print "Enn, you guess is not right<br>";
+    }
