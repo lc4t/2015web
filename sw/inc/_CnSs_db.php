fc6d9245c428236f1c4cdefbb5b78f5168f9c0f9
lc4t <lc4t0.0@gmail.com>
2015-09-29 22:57:46 +0800

    1

--- sw/inc/_CnSs_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $mysql_host = 'localhost';
-    $mysql_user = 'cnss';
-    $mysql_pass = 'cnss@lc4t';
-    $mysql_db   = 'cnss';
-?>
---
+++ xss/index.php
@@ -0,0 +1,16 @@
+<?php
+    
+?>
+
+
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <title>
+      xss it
+    </title>
+    <script src="js/jquery.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.js"></script>
+  </head>
+  
